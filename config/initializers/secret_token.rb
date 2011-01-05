a36466cda43cf9ee882303d4a9fb02ad13a9e89e
betten <betten@bc.edu>
2011-01-05 14:52:30 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kazanie::Application.config.secret_token = 'de3b7697a5c0ae8b619d4f806552e74e13fe332328bb367a61bfb2caf5ce23d92bef77761585dc73e79e6f24d13b450f4df2931638afb6b348c7ff67e0e2b6c4'
