adae49f8e69e047d038f47debbabd97bbc9de5cd
betten <betten@bc.edu>
2011-01-08 13:57:40 -0500

    generated controllers, implemented haml layout framework, added 960 css, started implementing layout/project views

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,6 +8,18 @@ class Ability
       can :manage, :all
     else
       can :read, :all
+
+      can :create, Project
+      can :create, Work do |work|
+        work.project.user.eql?(user)
+      end
+      can :create, Hotspot do |hotspot|
+        hotspot.work.user.eql?(user)
+      end
+
+      can :manage, Project, :user_id => user.id
+      can :manage, Work, :user_id => user.id
+      can :manage, Hotspot, :user_id => user.id
     end
   end
 end
