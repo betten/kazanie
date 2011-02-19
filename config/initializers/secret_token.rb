b560b8d17c02f5175e3d310e254e92ee60a168a9
betten <betten@bc.edu>
2011-02-18 22:35:16 -0500

    adding rails backend; added gems, added devise, generated devise user, generated project model/controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kazanie::Application.config.secret_token = 'e26c5d743315ca036f407d189fa83f021e22bcc3b9925702061e3647fea8f73da551a9436c08b3cda506db1573dbc96e158cadb2a85e96bf6f1be54a9a11794a'
