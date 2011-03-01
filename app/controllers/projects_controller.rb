49294cf65d9f612e4bc1e9f5e328f31e7b2bf2c4
betten <betten@bc.edu>
2011-03-01 13:11:48 -0500

    updated show page links and error css

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   def index
-    redirect_to admin_projects_path if user_signed_in?
+    redirect_to admin_projects_path, :notice => flash[:notice], :alert => flash[:alert]  if user_signed_in?
     @projects = Project.all.paginate :page => params[:page], :per_page => 10
   end
 
